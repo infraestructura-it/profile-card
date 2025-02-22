a104fb036aee101bfa4d416cd7568572d9f28b28
Jairo Sepulveda C <30697670+infraestructura-it@users.noreply.github.com>
2025-02-21 19:06:19 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://engjurado.tech',
+  site: 'https://infraestructura-it.github.io/profile-card/',
 });
